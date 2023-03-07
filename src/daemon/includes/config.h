aa6065e51af2bbc839a6b23610e7d057ffd01879
Tiago M <tiagodeha@protonmail>
2023-03-07 16:14:37 +0000

    Added unit tests to verify detecting programs (Windows only)

--- src/daemon/includes/config.h
+++ src/daemon/includes/config.h
@@ -1,6 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-static char *programs[] = {"OneNote.exe", "Notion.exe"};
+static char *programs[] = {"OneNote.exe"};
 
 #endif
--- src/daemon/tests/test_main.c
+++ src/daemon/tests/test_main.c
@@ -1,9 +1,55 @@
 #include "config.h"
+#include "daemon.h"
 #include <stdio.h>
+#include <assert.h>
+#include <windows.h>
+
+int test_programs_array(){
+    int size = sizeof(programs) / sizeof(programs[0]);
+    assert(size == 1);
+
+    return 0;
+}
+
+int test_find_pid(){
+    int size = sizeof(programs) / sizeof(programs[0]);
+    int pid, i;
+    char cmd[50];
+    
+    for(i = 0; i < size; i++){
+        memset(cmd, 0, 50);
+
+        sprintf(cmd, "start %s", programs[i]);   
+        system(cmd);
+
+        // Check if the process exists
+        pid = get_process_list(programs[i]);
+        assert(pid > 0);
+
+        // Kill program
+        memset(cmd, 0, 50);
+        sprintf(cmd, "taskkill /PID %d /F", pid);
+        system(cmd); 
+
+         // Check if the process exists
+        pid = get_process_list(programs[i]);
+        assert(pid == -1);
+    }
+
+    return 0;
+}
 
 int main(int argc, char **argv){
+    int ret;
+    
+    ret = test_programs_array();
+    
+    if(ret == 0)
+        printf("[+] SUCCESS on verify wanted programs!\n");
 
-    printf("Hello tests!\n");
+    ret = test_find_pid();
 
+    if(ret == 0)
+        printf("[+] SUCCESS on detecting programs!\n");
     return 0;
 }
